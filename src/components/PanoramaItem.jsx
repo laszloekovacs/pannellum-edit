990316f8f707e0e30d99bf6d51290d0e2b7b6d6f
laszloekovacs <laszloekovacs@gmail.com>
2023-03-12 19:23:13 +0100

    added add to scene button

--- src/components/PanoramaItem.jsx
+++ src/components/PanoramaItem.jsx
@@ -1,10 +1,23 @@
 import React from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { createScene } from "../state/projectReducer"
 
 const PanoramaItem = ({ item, url }) => {
+  const dispatch = useDispatch()
+  const scenes = Object.keys(useSelector((state) => state.project.scenes))
+
   return (
     <li>
-      <img src={url} alt="item" />
+      <img src={url} alt={url} />
       <p>{item}</p>
+      <button
+        disabled={scenes.includes(item)}
+        onClick={() => {
+          dispatch(createScene({ title: item }))
+        }}
+      >
+        add
+      </button>
     </li>
   )
 }
